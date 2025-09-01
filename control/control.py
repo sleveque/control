23c5bb03d9badf657fe140b7a66be5f5fe74a33a
sleveque <sleveque@central.uh.edu>
2025-09-01 16:57:11 +0100

    remove axpy from Instationary

--- control/control.py
+++ control/control.py
@@ -1757,23 +1757,16 @@ class Instationary:
                                b_help.copy(deepcopy=True))
 
                 for i in range(1, n_t - 1):
-                    block_ij = block_10[(i, i - 1)]
+                    block_ij = block_10[(i, i - 1)] + my_const * self._M_v
                     b_help = Function(self.space_v)
                     b_help.assign(u_1.sub(i - 1))
                     b_help_new = assemble(action(block_ij, b_help))
                     with b.sub(i).dat.vec as b_v, \
                             b_help_new.dat.vec_ro as b_1_v:
                         b_v.axpy(-1.0, b_1_v)
-                    b_help_new = assemble(
-                        action(my_const * self._M_v,
-                               b_help))
-                    with b.sub(i).dat.vec as b_v, \
-                            b_help_new.dat.vec_ro as b_1_v:
-                        b_v.axpy(-1.0, b_1_v)
                     apply_bcs(bcs_zeta, b.sub(i))
                     b_help = Cofunction(self.space_v.dual())
                     b_help.assign(b.sub(i))
-
                     solver_1 = solver_state[(i)]
                     u_1.sub(i).zero()
                     solver_1.solve(u_1.sub(i),
